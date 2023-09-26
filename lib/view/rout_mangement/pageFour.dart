1f802197d412d39834f5ac7ff2946569417e2812
abo1od <bestdesigner1111@gmail.com>
2023-09-26 17:52:09 +0300

    page four the secode version

---
+++ lib/view/rout_mangement/pageFour.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PageFour extends StatelessWidget {
+  const PageFour({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page Four')),
+      body: Center(
+          child: Column(
+        children: [const Text('the body of page four ')],
+      )),
+    );
+  }
+}
